9e1bc1aa8ebd293ae44718b9e9fcd013bcddc70d
daichi3102 <k.daichi.p3@gmail.com>
2024-04-24 19:54:50 +0900

    answerのビューについての修正

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -22,10 +22,6 @@ class AnswersController < ApplicationController
 
   private
 
-  def set_question
-    @question = Question.find(params[:question_id])
-  end
-
   def answer_params
     params.require(:answer).permit(:content).merge(question_id: params[:question_id])
   end
