6e81c2c8393042a121417a8750ba4466a89dd87b
atsushi59 <atsushiyamada59@gmail.com>
2024-04-24 14:56:45 +0900

    パスワード変更の実装

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,6 +45,9 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  config.action_mailer.delivery_method = :letter_opener #letter openerを使用する際に必要
+  config.action_mailer.perform_deliveries = true  #letter openerを使用する際に必要
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Rails.application.routes.draw do
   resources :questions, only: %i[index new create show edit update destroy] do
     resources :answers, only: %i[create destroy], shallow: true
   end
-  root to: "questions#index"
-  resources :questions
   resource :profiles
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   # get 'tops/index'
@@ -16,6 +14,8 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development? #letter openerへのルーティング
 
   # Defines the root path route ("/")
   # root "posts#index"
