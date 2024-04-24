fd84b7e8a8ea5360667271b7e97c8e70c946b026
daichi3102 <k.daichi.p3@gmail.com>
2024-04-24 19:42:00 +0900

    answerのビューについて修正

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,7 +1,10 @@
 class AnswersController < ApplicationController
+  def new
+    @answer = @question.answers.build
+  end
+
   def create
     answer = current_user.answers.build(answer_params)
-    binding.pry
     if  answer.save
       flash[:success] = '投稿に成功しました'
       redirect_to question_path(answer.question), success: t('defaults.flash_messages.created', item: Answer.model_name.human)
@@ -18,6 +21,10 @@ class AnswersController < ApplicationController
   end
 
   private
+
+  def set_question
+    @question = Question.find(params[:question_id])
+  end
 
   def answer_params
     params.require(:answer).permit(:content).merge(question_id: params[:question_id])
